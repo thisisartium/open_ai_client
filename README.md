bdd26593706e976440aaa0dd718629f29a5383f3
John Wilger <john@johnwilger.com>
2023-10-31 15:47:37 -0700

    Refactoring and documentation

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # OpenAiClient
 
-**TODO: Add description**
+OpenAiClient is a client for the OpenAI API. It supports all options provided by
+the `Req` library, as well as additional options such as a circuit breaker
+module (defaults to `ExBreak`) and the OpenAI organization ID.
 
 ## Installation
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `open_ai_client` to your list of dependencies in `mix.exs`:
+The package can be installed by adding `open_ai_client` to your list of
+dependencies in `mix.exs`:
 
 ```elixir
 def deps do
@@ -15,7 +17,29 @@ def deps do
 end
 ```
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at <https://hexdocs.pm/open_ai_client>.
+## Configuration
 
+In your `config/runtime.exs` file, you need to set up the OpenAI API key:
+
+```elixir
+config :open_ai_client, OpenAiClient, openai_api_key: System.get_env("OPENAI_API_KEY")
+```
+
+## Usage
+
+You can send a POST request to the OpenAI API like this:
+
+```elixir
+OpenAiClient.post("https://api.openai.com/v1/chat/completions", [json: %{model: "gpt-3.5-turbo", messages: [%{role: "system", content: "You are a helpful assistant."}, %{role: "user", content: "Who won the world series in 2020?"}]}])
+```
+
+And a GET request like this:
+
+```elixir
+OpenAiClient.get("https://api.openai.com/v1/models")
+```
+
+Documentation can be generated with
+[ExDoc](https://github.com/elixir-lang/ex_doc) and published on
+[HexDocs](https://hexdocs.pm). Once published, the docs can be found at
+<https://hexdocs.pm/open_ai_client>.
--- lib/open_ai_client.ex
+++ lib/open_ai_client.ex
@@ -1,38 +1,53 @@
 defmodule OpenAiClient do
   @moduledoc """
   A client for the OpenAI API.
+
+  This client supports all options provided by the `Req` library, as well as additional options:
+  - :breaker - a circuit breaker module (default: `ExBreak`)
+  - :openai_organization - the OpenAI organization ID
   """
 
   @doc """
   Sends a POST request to the OpenAI API.
+
+  ## Examples
+
+      iex> OpenAiClient.post("https://api.openai.com/v1/chat/completions",
+      ...> [json: %{model: "gpt-3.5-turbo", messages: [%{role: "system", content: "You are a helpful assistant."}, %{role: "user", content: "Who won the world series in 2020?"}]}])
+      {:ok, %{"id" => "chatcmpl-3o4Bz2rawi6wk8c4L8QR4W7eFCDj", "object" => "chat.completion", "created" => 1646183485, "model" => "gpt-3.5-turbo", "choices" => [%{"message" => %{ "role" => "assistant", "content" => "The Los Angeles Dodgers won the World Series in 2020."}, "finish_reason" => "stop", "index" => 0}]}}
+
   """
   @spec post(String.t(), Keyword.t()) :: {:ok, map()} | {:error, any()}
   def post(url, options) do
-    request(:post, url, options)
+    send_request(:post, url, options)
   end
 
   @doc """
   Sends a GET request to the OpenAI API.
+
+  ## Examples
+
+      iex> OpenAiClient.get("https://api.openai.com/v1/models")
+      {:ok, %{"models" => [%{"id" => "gpt-3.5-turbo", "object" => "model", "created" => 1646183485, "name" => "gpt-3.5-turbo"}]}}
+
   """
   @spec get(String.t(), Keyword.t()) :: {:ok, map()} | {:error, any()}
   def get(url, options) do
-    request(:get, url, options)
+    send_request(:get, url, options)
   end
 
-  @spec request(atom(), String.t(), Keyword.t()) :: {:ok, map()} | {:error, any()}
-  defp request(method, url, options) do
+  defp send_request(method, url, options) do
     breaker = Keyword.get(options, :breaker, ExBreak)
-
     options = Keyword.delete(options, :breaker)
 
-    breaker.call(&OpenAiClient.__do_request__/3, [method, url, options],
+    breaker.call(&__MODULE__.__do_request__/3, [method, url, options],
       threshold: 10,
       timeout_sec: 120,
-      match_return: fn {:ok, response} -> breaker(response) end
+      match_return: &breaker_response/1
     )
   end
 
-  defp breaker(response) do
+  defp breaker_response(response) do
     case response do
       %{status: status} when status in 500..599 -> true
       %{status: 429} -> true
@@ -47,11 +62,17 @@ defmodule OpenAiClient do
     |> Req.request()
   end
 
-  @spec build_request(String.t(), Keyword.t()) :: map()
   defp build_request(url, options) do
     options
     |> Keyword.put(:url, url)
-    |> Keyword.validate!([
+    |> validate_options()
+    |> set_headers()
+    |> remove_nil_values()
+    |> Req.new()
+  end
+
+  defp validate_options(options) do
+    Keyword.validate!(options, [
       :url,
       :json,
       :method,
@@ -62,34 +83,29 @@ defmodule OpenAiClient do
       base_url: default_base_url(),
       auth: {:bearer, default_api_key()}
     ])
-    |> set_headers()
-    |> Enum.reject(fn {_key, value} -> value == nil end)
-    |> Req.new()
   end
 
-  @spec default_base_url() :: String.t()
+  defp remove_nil_values(options) do
+    Enum.reject(options, fn {_key, value} -> value == nil end)
+  end
+
   defp default_base_url do
     Application.get_env(:open_ai_client, OpenAiClient)[:base_url]
   end
 
-  @spec default_api_key() :: String.t()
   defp default_api_key do
     Application.get_env(:open_ai_client, OpenAiClient)[:openai_api_key]
   end
 
-  @spec default_organization() :: String.t()
   defp default_organization do
     Application.get_env(:open_ai_client, OpenAiClient)[:openai_organization_id]
   end
 
-  @spec set_headers(Keyword.t()) :: Keyword.t()
   defp set_headers(options) do
     headers =
-      if options[:openai_organization] do
-        [{"openai-organization", options[:openai_organization]}]
-      else
-        []
-      end
+      if options[:openai_organization],
+        do: [{"openai-organization", options[:openai_organization]}],
+        else: []
 
     options
     |> Keyword.put_new(:headers, headers)
